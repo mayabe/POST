5a1f9175fd08c4d780a6d937be2b0ac96275e06e
mayabe <mayasaurusrex@Mayas-MacBook-Pro.local>
2015-02-08 19:39:35 -0800

    adjustment to payment class for credit card number

--- src/mastopay/Payment.java
+++ src/mastopay/Payment.java
@@ -3,6 +3,8 @@
  */
 package mastopay;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @author tenzinwangchuk
@@ -13,40 +15,40 @@ public class Payment {
      *
      */
     public enum PayType {
-        
-        /**
-         *
-         */
         CASH,
-        
-        /**
-         *
-         */
         CREDIT,
-        
-        /**
-         *
-         */
-        CHECK };
+        CHECK
+    };
     
     private PayType type;
-    private int amountTotal;
+    private double amountTotal;
+    private String cardNumber;
     
     /**
      *
      */
-    public Payment() { }
+    public Payment() {
+        type = null;
+        amountTotal = 0;
+        cardNumber = "";
+    }
     
-    public Payment(int amtTotal, PayType pType) {
-        amountTotal = amtTotal;
+    public Payment(PayType pType) {
         type = pType;
+        amountTotal = 0;
+        cardNumber = "";
+    }
+    
+    public Payment(Payment pay) {
+        amountTotal = pay.getAmountTotal();
+        type = pay.getPayType();
     }
     
     /**
      *
      * @param total
      */
-    public void setAmountTotal(int total) {
+    public void setAmountTotal(double total) {
         this.amountTotal = total;
     }
     
@@ -54,11 +56,24 @@ public class Payment {
         type = t;
     }
     
+    public void setCardNumber(String card) {
+        if(type == PayType.CREDIT) {
+            if(Pattern.matches("\\d{5}", card)) {
+                cardNumber = card;
+            } else {
+                System.err.println("Error: setCardNumber() only accepts input of a string of 5 digits");
+            }
+        } else {
+            System.err.println("Error: Cannot set cardNumber for payment of type " + type);
+        }
+        
+    }
+    
     /**
      *
      * @return amountTotal
      */
-    public int getAmountTotal() {
+    public double getAmountTotal() {
         return amountTotal;
     }
     
@@ -68,6 +83,10 @@ public class Payment {
      */
     public PayType getPayType() {
         return type;
+    }
+    
+    public String getCardNumber() {
+        return cardNumber;
     }
     
 }
